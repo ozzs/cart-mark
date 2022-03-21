ac478dca29e49ce1c4d7aa9fcbe5c63b18e086f3
ozzs <ozzs@users.noreply.github.com>
2022-03-21 23:57:28 +0200

    responsive navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,26 +1,21 @@
-import React, {useRef} from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
-import { FaBars, FaTimes } from 'react-icons/fa'
+import { FaBars } from 'react-icons/fa'
 import './Navbar.css';
 
 function Navbar() {
 
-    const navRef = useRef();
-
-    const showNavbar = () => {
-        navRef.current.classList.toggle("responsive-nav");
-    }
+    const [showNavbar, setShowNavbar] = useState(false);
 
     return (
         <header>
-            <nav ref={navRef}>
+            <nav className={showNavbar ? 'hidden-navbar' : ''}>
                 <NavLink to="/" className='link' activeclassname='active'> Home </NavLink>
                 <NavLink to="/additem" className='link' activeclassname='active'> Add Item </NavLink>
                 <NavLink to="/createlist" className='link' activeclassname='active'> Create List </NavLink>
                 <NavLink to="/shoppinglist" className='link' activeclassname='active'> Go Shopping! </NavLink>
-                <FaTimes className='nav-btn nav-close-btn' onClick={showNavbar}/>
             </nav>
-            <FaBars className='nav-btn' onClick={showNavbar}/>
+            <FaBars className='nav-btn' onClick={() => setShowNavbar(!showNavbar)} />
         </header>
     )
 }

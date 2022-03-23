a2f5125ca95ac7b8bc3013dadadb674ac42a1c97
ozzs <ozzs@users.noreply.github.com>
2022-03-24 00:59:44 +0200

    Revert "responsive navbar"
    This reverts commit ac478dca29e49ce1c4d7aa9fcbe5c63b18e086f3.

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,21 +1,26 @@
-import React, { useState } from 'react'
+import React, {useRef} from 'react'
 import { NavLink } from 'react-router-dom'
-import { FaBars } from 'react-icons/fa'
+import { FaBars, FaTimes } from 'react-icons/fa'
 import './Navbar.css';
 
 function Navbar() {
 
-    const [showNavbar, setShowNavbar] = useState(false);
+    const navRef = useRef();
+
+    const showNavbar = () => {
+        navRef.current.classList.toggle("responsive-nav");
+    }
 
     return (
         <header>
-            <nav className={showNavbar ? 'hidden-navbar' : ''}>
+            <nav ref={navRef}>
                 <NavLink to="/" className='link' activeclassname='active'> Home </NavLink>
                 <NavLink to="/additem" className='link' activeclassname='active'> Add Item </NavLink>
                 <NavLink to="/createlist" className='link' activeclassname='active'> Create List </NavLink>
                 <NavLink to="/shoppinglist" className='link' activeclassname='active'> Go Shopping! </NavLink>
+                <FaTimes className='nav-btn nav-close-btn' onClick={showNavbar}/>
             </nav>
-            <FaBars className='nav-btn' onClick={() => setShowNavbar(!showNavbar)} />
+            <FaBars className='nav-btn' onClick={showNavbar}/>
         </header>
     )
 }

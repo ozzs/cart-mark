26a33d0f0e5be0ac8b4351d205b33404e5233f80
ozzs <ozzs@users.noreply.github.com>
2022-05-05 11:17:40 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ const port = 5000
 const sqlite3 = require('sqlite3').verbose();
 
 const db = new sqlite3.Database('./shopListDB.db', sqlite3.OPEN_READWRITE, (err) => {
-  if(err) 
-    return console.error(err.message);
+  if(err) {
+    console.error(err.message);
+    return;
+  }
 
   console.log('connection with database successful');
 });

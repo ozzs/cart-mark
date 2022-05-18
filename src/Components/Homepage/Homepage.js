345ba3ec653fd4993651a560dc700a4183c12d45
ozzs <ozzs@users.noreply.github.com>
2022-05-19 00:58:30 +0300

    homepage update

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -16,6 +16,7 @@ function Homepage() {
         />
       </div>
 
+      <h2 className="page-desc"> Pages Description </h2>
       <div className="boxes-container">
         <div className="box">
           <AiFillFileAdd className="icon" /> <br />
@@ -55,6 +56,15 @@ function Homepage() {
             View your previous lists, sorted by dates.{" "}
           </div>
         </div>
+      </div>
+
+      <h2 className="reason"> Why do I even need it? </h2>
+      <div className="description">
+        <p> Have you ever felt tired of making a careless shopping list? Scribble a few groceries on a note and then lose it? Or maybe get a shopping list from your wife and not understand what is written in it and what I am supposed to bring? Wait, did she want almond milk or soy milk? </p> <br />
+        <p> As a computer science student who rents an apartment with a partner and does shopping once or twice a week, I was looking for an idea for a project that would help me solve the problems mentioned and also acquire new tools that would expand my knowledge. </p> <br />
+        <p> With this website, you can create such a shopping list, so that you do not have to deal with most of the issues raised earlier. The application is structured in such a way that it can be used on a computer or mobile devices. </p> <br />
+        <p> You would be able to create a shopping list from a product catalog (which you also make yourself), edit and add comments to it. Once the list is ready you can open it and mark exactly what has been taken from the shelf and what has not. At the end of the purchase the list will be saved and you can view it again later. </p> <br />
+        And most importantly, you will no longer be confused with almond milk and soy milk!
       </div>
     </>
   );
